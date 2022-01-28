56cf87eacfc3e9354c096aa1e57d362b41429d7a
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-01-28 16:43:40 +0100

    Finished first part with styling and removing alerts

---
+++ script.js
@@ -0,0 +1,36 @@
+const alerts = [
+  "Unable to access resource. User is not authorized.",
+  "You are missing proper user permissions.",
+  "Administrators have been notified of this action.",
+  "Please remain where you are, police is on the way.",
+];
+
+const body = document.querySelector("body");
+
+body.innerHTML = "<div class='alerts'></div>";
+
+const container = document.querySelector(".alerts");
+console.log(container);
+
+alerts.forEach((element) => {
+  console.log(element);
+  container.innerHTML += `<div class="alert"><div class="alert__text">${element}</div><div class="alert__close">&times;</div></div>`;
+});
+
+const alertElements = Array.from(document.querySelectorAll(".alert"));
+
+console.log(alertElements);
+
+alertElements.forEach((element) => {
+  element.addEventListener("click", () => {
+    element.style.backgroundColor = "green";
+    element.style.color = "white";
+  });
+});
+
+const alertCloses = Array.from(document.querySelectorAll(".alert__close"));
+alertCloses.forEach((element) => {
+  element.addEventListener("click", () => {
+    element.parentNode.style.display = "none";
+  });
+});
